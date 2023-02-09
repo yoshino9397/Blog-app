9bb066b600e29d3f1f04d342d09bd12c892ef22f
yoshino9397 <yosino8512@gmail.com>
2023-02-08 20:18:53 -0800

    fix mysql

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,31 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/auth/login", inputs);
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
   return (
     <div className="auth">
       <h1>Login</h1>
@@ -10,23 +35,21 @@ const Login = () => {
           type="text"
           placeholder="username"
           name="username"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           required
           type="password"
           placeholder="password"
           name="password"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
-        {/* <button onClick={handleSubmit}>Login</button> */}
-        <button>Login</button>
-        {/* {err && <p>{err}</p>}
-         */}
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p>{err}</p>}
         <span>
-          Don't you have an account?
-          <Link to="/register" style={{ textDecoration: "none" }}>
-            <em>Register</em>
+          Do you have an account?
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            <em>Login</em>
           </Link>
         </span>
       </form>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,31 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/auth/register", inputs);
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
   return (
     <div className="auth">
       <h1>Register</h1>
@@ -10,26 +35,24 @@ const Register = () => {
           type="text"
           placeholder="username"
           name="username"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           required
           type="email"
           placeholder="email"
           name="email"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           required
           type="password"
           placeholder="password"
           name="password"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
-        {/* <button onClick={handleSubmit}>Register</button> */}
-        <button>Register</button>
-        {/* {err && <p>{err}</p>}
-         */}
+        <button onClick={handleSubmit}>Register</button>
+        {err && <p>{err}</p>}
         <span>
           Do you have an account?
           <Link to="/login" style={{ textDecoration: "none" }}>
