9278ea6467273749c6c9d95db3c5bafc1596a930
yoshino9397 <yosino8512@gmail.com>
2023-02-09 18:20:56 -0800

    authContext DONE

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import authRoute from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 // app.get("/test",(req, res) => {
 //   res.json("OKAY")
